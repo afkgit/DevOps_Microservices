09b8b8a70398fb14205bdaaf0546887aaf11aade
AFK <nifty_afk@yahoo.com>
2022-07-29 05:35:06 +0000

    circleci updated

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,7 +8,9 @@
 dockerpath="niftyafk/flaskprj:v1.0"
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run niftyafk --image=niftyafk/flaskprj:v1.0 --port=80 --labels app=flaskprj
+kubectl run flaskprj \
+--image=niftyafk/flaskprj:v1.0 \
+--port=80 --labels app=flaskprj
 
 # Step 3:
 # List kubernetes pods
