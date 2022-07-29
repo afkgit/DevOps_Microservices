a99352850ebada0876db5800ec267dc667be7370
AFK <nifty_afk@yahoo.com>
2022-07-29 03:38:28 +0000

    run_k8s

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="niftyafk/flaskprj:v1.0"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run niftyafk --image=niftyafk/flaskprj:v1.0 --port=80 --labels app=flaskprj
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward flaskprj 8000:80 
